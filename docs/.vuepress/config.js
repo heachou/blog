2644b6258480af742f55463d12188b1c8a2eca15
heachou <1244239990@qq.com>
2020-08-05 11:25:37 +0800

    文档新增

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: '文字记录学习的过程',
   theme: 'reco',
   head: [
-    ['link', { rel: 'icon', href:'/logo.ico'}],
+    ['link', { rel: 'icon', href: '/logo.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
   themeConfig: {
@@ -17,7 +17,8 @@ module.exports = {
       { text: 'python', link: '/python/' },
     ],
     sidebar: {
-      '/react/': ['', '基础', '生命周期', 'hooks']
+      '/react/': ['', '基础', '生命周期', 'hooks'],
+      '/vue/': ['', 'vuepress配置code-copy']
     },
     // 博客配置
     blogConfig: {
@@ -29,6 +30,10 @@ module.exports = {
         location: 3,     // 在导航栏菜单中所占的位置，默认3
         text: 'Tag'      // 默认文案 “标签”
       }
-    }
-  }
+    },
+  },
+  plugins: [['vuepress-plugin-code-copy', {
+    selector: 'div[class*="language-"] pre',
+    color: '#3e7f7c'
+  }]]
 }
---
+++ docs/vue/vuepress配置code-copy.md
@@ -0,0 +1,28 @@
+---
+title: vuepress 配置code-copy 插件
+date: 2020-08-05
+categories:
+ - 前端
+tags:
+ - vue
+ - vuepress
+---
+
+# vuepress 配置code-copy 插件
+
+## 安装插件
+
+```bash
+npm install vuepress-plugin-code-copy
+# OR
+yarn add vuepress-plugin-code-copy
+```
+
+## 配置
+
+```js
+module.exports = {
+    plugins: [['vuepress-plugin-code-copy', true]]
+}
+```
+更多参考 [vuepress-plugin-code-copy](https://www.npmjs.com/package/vuepress-plugin-code-copy)
