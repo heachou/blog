fef65afb0446fb02ccf52c4859c28af67d5db96c
heachou <1244239990@qq.com>
2020-08-12 22:56:34 +0800

    again

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,7 +27,7 @@ module.exports = {
     // 博客配置
     blogConfig: {
       category: {
-        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        location: 2,     // 在导航栏菜单中所占的位置，默认为2
         text: 'Category' // 默认文案 “分类”
       },
       tag: {
